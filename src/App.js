819dec7aa019271b8e0c1c251be050cca4f59e42
yunkhngn <rogkhoa.mail@gmail.com>
2023-04-27 23:41:24 +0700

    Analyst

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import {
   Submit,
 } from "./components/hooks";
 import { useState } from "react";
-
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 
 function App() {
   const [show, setShow] = useState(false);
-  console.log(show)
+  console.log(show);
   return (
     <div className="App">
-      <HeadCard show={show} setShow={setShow}/>
-      <NameInput show={show}/>
-      <MultipleChoice show={show}/>
-      <Form show={show}/>
-      <ShortText show={show}/>
-      <Submit show={show} setShow={setShow}/>
+      <HeadCard show={show} setShow={setShow} />
+      <NameInput show={show} />
+      <MultipleChoice show={show} />
+      <Form show={show} />
+      <ShortText show={show} />
+      <Submit show={show} setShow={setShow} />
+      <Analytics/>
     </div>
   );
 }
